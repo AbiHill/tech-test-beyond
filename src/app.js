08aeda501322aa42237072099aabe8109fc79bac
AbiHill <abihill8@gmail.com>
2018-05-16 14:44:36 +0100

    indexRoute state created and mapping over array

--- src/app.js
+++ src/app.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import IndexRoute from './components/IndexRoute';
 import ShowRoute from './components/ShowRoute';
-
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 class App extends React.Component {
 
--- src/components/IndexRoute.js
+++ src/components/IndexRoute.js
@@ -1,9 +1,41 @@
 import React from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
-const IndexRoute = () => {
-  return(
-    <h1>IndexRoute</h1>
-  );
-};
+class IndexRoute extends React.Component {
+  state =  {
+    videos: {
+      items: []
+    }
+  }
+
+  componentWillMount() {
+    this.getVideos();
+  }
+
+  getVideos() {
+    axios.get('https://www.googleapis.com/youtube/v3/playlistItems?part=snippet,contentDetails,status&maxResults=10&playlistId=PLSi28iDfECJPJYFA4wjlF5KUucFvc0qbQ&key=AIzaSyCuv_16onZRx3qHDStC-FUp__A6si-fStw')
+      .then(res => this.setState({ videos: res.data}, () => console.log('get videos', this.state.videos.items)));
+  }
+
+  render() {
+    console.log('render', this.state.videos.items);
+    return (
+      <div>
+        <ul>
+          {this.state.videos.items.map((video, i) =>
+            <li key={i}>
+              <div>
+                <div>
+                  <h3>{video.kind}</h3>
+                </div>
+              </div>
+            </li>
+          )}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default IndexRoute;
